7237d920caac8c2cb0e7d941ee66c5b25bf33608
Naveen jose <121199481+silvernberry@users.noreply.github.com>
2024-02-27 13:43:14 +0530

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,7 @@
-# Build the project in release mode  
-RUN cargo build --release
+#!/bin/bash
 
-# Run the Substrate node in development mode . STRICTLY USED FOR DEVELOPMENT PURPOSE ONLY
-CMD ["./target/release/pocs", "--dev", "--rpc-external", "--rpc-cors=Unsafe"]
+# Build the project in release mode  
+cargo build --release
+
+# Run the Substrate node in development mode. STRICTLY USED FOR DEVELOPMENT PURPOSE ONLY
+./target/release/pocs --dev --rpc-external --rpc-cors=Unsafe
