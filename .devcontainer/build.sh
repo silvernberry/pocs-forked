01df501f0ed959e1e4086eda58232aead22bedbb
Naveen jose <121199481+silvernberry@users.noreply.github.com>
2024-02-28 03:10:20 +0530

    Update build.sh

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-# Get the current branch name
-echo "Getting the current branch name..."
-BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+# Build the project in release mode  
+cargo build --release
 
-# Print the branch name
-echo "Current branch: $BRANCH_NAME"
-
-# Build only for master branch
-if [ "$BRANCH_NAME" = "master" ]; then
-  echo "Master Branch verified. Building Node..."
-  cargo build --release
-else
-  echo "Not on master branch. Skipping build."
-fi
-
-# Restart bash for changes to take effect
-echo "Restarting bash for changes to take effect..."
-exec bash
-
-# Run the Substrate node in development mode . STRICTLY USED FOR DEVELOPMENT PURPOSE ONLY
-["./target/release/pocs", "--dev", "--rpc-external", "--rpc-cors=Unsafe"] &
+# Run the Substrate node in development mode. STRICTLY USED FOR DEVELOPMENT PURPOSE ONLY
+./target/release/pocs --dev --rpc-external --rpc-cors=Unsafe &
